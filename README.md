ef8023460bfab31980308987ae68c8292639ee21
Elijahthe3rd <sepuru.ne@yahoo.com>
2020-08-25 15:37:52 +0200

    refactored file book.js

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
+#### Tools
 
-        undefined
-        undefined
-        undefined
-        undefined
-        undefined
-        
+> * nodejs
+> * node fs module 
+> * vscode IDE
+
+#### Ways to run the project 
+1. clone the repo
+2. cd or navigate to the cloned dir / forlder named: NODEJS-FILEIO
+3. using your text editor any of your choice but i recommend vscode as this project is developed using vsc ide
+4. open the book.js file and uncomment the  
+5. From your keyboard press key Ctrl+\` or Ctrl+Shift+\` for new terrminal
+6. > Commands
--- book.js
+++ book.js
@@ -65,17 +65,31 @@ class Book{
 
 }
 
-
+//class that i created to render/give me random text as body mostly are attributes of array object with
 const jsonInstance=require('/home/recruit/Desktop/backup/Sepuru/LocalAPI/objects/object')
+
+//TESTS BELOW
+
+//Instanciating my class/creating object of of class and constructing it  
 let book=new Book(" not to the marriage of true minds","Shakes Spear","LondonP.K",new Date().toLocaleDateString(),jsonInstance.render())
 
-// book.createFile("dela-cream")
-// book.readFile("dela-cream2")
-// book.createFile("dela-cream2")
-// book.readFile("dela-cream2")
-// book.deleteFile("dela-cream2")
-// book.UpdateFile("dela-cream",book)
-// book.UpdateFile("dela-cream",new Book("my Life love","Blood line barrer","K.G.M",new Date().toLocaleDateString(),jsonInstance.render()))
-
+/* (N.B) the following are the tests i used to run my project
+ * please check each method / function before using my tests 
+ *
+ * book.readFile("dela-cream2") //upgraded to the bellow
+ * book.readFile(path,fileName)
+ * 
+ * book.createFile("dela-cream2")//upgraded to the bellow
+ * book.createFile(path,fileName)
+ * 
+ * book.deleteFile("dela-cream2")
+ * 
+ * OPTION.1 before upgrades)
+ * book.UpdateFile("dela-cream",book)//upgraded to the bellow
+ * OPTION.2)
+ * book.UpdateFile(path,fileName,data(.e.g book))
+ * OPTION.3) 
+ * book.UpdateFile(path,fileName,"dela-cream",new Book("my Life love","Blood line barrer","K.G.M",new Date().toLocaleDateString(),jsonInstance.render()))
+ */
 
 module.exports={book}
